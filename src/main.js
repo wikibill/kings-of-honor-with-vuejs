63b534d60d4406e84f9a9756c6935ffd44255dbe
顶顶顶 <1689336897@qq.com>
2020-09-21 21:29:23 +0800

    items start

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+import 'swiper/swiper-bundle.css'
+// import 'swiper/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
+
+
+
+import '@/assets/sass/style.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <swiper ref="mySwiper" :options="swiperOptions">
+      <swiper-slide>
+        <img src="@/assets/images/swiper-1.jpeg" class="w-100" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="@/assets/images/swiper-2.jpeg" class="w-100" />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="@/assets/images/swiper-3.jpeg" class="w-100" />
+      </swiper-slide>
+      <div class="swiper-pagination pagination-home" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: ".pagination-home",
+        },
+      },
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.$swiper;
+    },
+  },
+  mounted() {
+    // console.log("Current Swiper instance object", this.swiper);
+    // this.swiper.slideTo(3, 1000, false);
+  },
+};
+</script>
+
+<style lang="scss">
+.pagination {
+  .swiper-pagination-bullet {
+    background-color: #fff;
+  }
+}
+</style>
